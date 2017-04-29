7040bc33afa0030e029ae9afe81c2f0ae8906aff
Darren Richardson <noreply@mnestis.net>
2017-04-29 19:52:41 +0100

    Basic project layout.

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name="sccminutes",
+    version="0.0.1",
+    author="Darren Richardson",
+    description="Monitor the minutes of Southampton City Council.",
+    license="MIT",
+    url="https://github.com/mnestis/soton-city-council",
+    packages=["sccminutes"],
+    scripts=["bin/sccminutes"],
+)
